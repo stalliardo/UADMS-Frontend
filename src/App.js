23c27a215454fa38d3474e11b5b69480bd2493f4
stalliardo <46523228+stalliardo@users.noreply.github.com>
2024-12-07 12:08:27 +0000

    Added the navbar and some basic styles (#1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const Home = () => {
   return <div>Home</div>
@@ -8,6 +9,7 @@ const Home = () => {
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
